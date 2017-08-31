9398754c1c84bebe8cab456e8a7a87ff4fd53223
MikeAinOz <mike@mikeallen.com.au>
2017-09-01 06:04:41 +1000

    Create Example File
    Example for Brian

---
+++ CryptoTickerExample.ino
@@ -0,0 +1,149 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#include <CoinMarketCapApi.h>
+
+/*******************************************************************
+ *  An example of getting ticker info from coinmarketcap.com       *
+ *                                                                 *
+ *  Good bits Written by Brian Lough
+ *  Hacked by Mike Allen for   Huzzah FeatherWing 128x32 OLED                                  *
+ *******************************************************************/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+//------- Install From Library Manager -------
+#include <ArduinoJson.h>
+
+
+//------- Replace the following! ------
+char ssid[] = "SSID";       // your network SSID (name)
+char password[] = "PASSWORD";  // your network key
+
+
+WiFiClientSecure client;
+CoinMarketCapApi api(client);
+
+// CoinMarketCap's limit is "no more than 10 per minute"
+// Make sure to factor in if you are requesting more than one coin.
+unsigned long api_mtbs = 60000; //mean time between api requests
+unsigned long api_due_time = 0;
+
+Adafruit_SSD1306 display = Adafruit_SSD1306();
+
+#if defined(ESP8266)
+  #define BUTTON_A 0
+  #define BUTTON_B 16
+  #define BUTTON_C 2
+  #define LED      0
+#elif defined(ARDUINO_STM32F2_FEATHER)
+  #define BUTTON_A PA15
+  #define BUTTON_B PC7
+  #define BUTTON_C PC5
+  #define LED PB5
+#elif defined(TEENSYDUINO)
+  #define BUTTON_A 4
+  #define BUTTON_B 3
+  #define BUTTON_C 8
+  #define LED 13
+#else 
+  #define BUTTON_A 9
+  #define BUTTON_B 6
+  #define BUTTON_C 5
+  #define LED      13
+#endif
+#define SSD1306_LCDHEIGHT 32
+#if (SSD1306_LCDHEIGHT != 32)
+ #error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was Previously
+  // connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  IPAddress ip = WiFi.localIP();
+  Serial.println(ip);
+   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  Serial.println("OLED begun");
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(1000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+  
+  Serial.println("IO test");
+
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+
+}
+
+
+void displayTickerData(String ticker) {
+  
+  // Ticker unfortunately is not the symbol for some reason.
+  // Go to CoinMarketCap.com and select the coin you would like to check
+  // The ticker name makes up the last part of the URL
+  // e.g: http://coinmarketcap.com/currencies/bitcoin/ , "bitcoin" is the ticker value
+
+  // Currency is optional, so you can pass only ticker if you want.
+  // Check out the currency drop down on CoinMarketCap.com to get available values
+  CMCTickerResponse response = api.GetTickerInfo(ticker, "eur");
+  if (response.error == "") {
+   
+    display.clearDisplay();
+    display.display();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println(response.name);
+    display.setTextSize(1);
+    display.println(" ");
+    display.setTextSize(3);
+    display.println(response.price_usd);
+    display.setTextSize(2);
+    display.print(response.percent_change_24h);
+    display.println("% 24hr.");
+    display.display();
+  } else {
+    display.print("Error getting data: ");
+    display.println(response.error);
+    display.display();
+  }
+  
+}
+void loop() {
+  
+ //   displayTickerData("bitcoin");
+    displayTickerData("ethereum");
+ //   displayTickerData("dogecoin");
+    delay(60000);
+}
